dec0f7aa636d0e30912819e56ecb5be6fd92c9d8
Pedro Lucas Bezerra <pedro.lucas210600@gmail.com>
2020-06-05 14:49:17 -0300

    App

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -1,44 +1,56 @@
-import { Request, Response, response } from 'express';
+import { Request, Response } from 'express';
 import knex from '../database/connection';
 
 class PointsController {
 
-  async index(req: Request, res: Response){
+  async index(req: Request, res: Response) {
     const { city, uf, items } = req.query;
 
     const parsedItems = String(items)
-    .split(',')
-    .map(item => Number(item.trim()))
-    
+      .split(',')
+      .map(item => Number(item.trim()))
+
     const points = await knex('points')
       .join('point_items', 'points.id', '=', 'point_items.point_id')
       .whereIn('point_items.item_id', parsedItems)
       .where('city', String(city))
       .where('uf', String(uf))
-    
+
       .select('points.*');
 
-    return res.json(points);
+    const serializedPoints = points.map(point => {
+      return {
+        ...point,
+        image_url: `http://192.168.0.104:3333/uploads/${point.image}`,
+      };
+    });
+
+    return res.json(serializedPoints);
   }
 
-  async show(req: Request, res: Response){
+  async show(req: Request, res: Response) {
     const { id } = req.params;
 
     const point = await knex('points').where('id', id).first();
 
-    if(!point){
+    if (!point) {
       return res.status(400).json({ message: 'Point not found.' });
     }
+
+    const serializedPoint =  {
+        ...point,
+        image_url: `http://192.168.0.106:3333/uploads/${point.image}`,
+    };
 
     const items = await knex('items')
       .join('point_items', 'items.id', '=', 'point_items.item_id')
       .where('point_items.point_id', id)
       .select('items.title');
 
-    return res.json({ point, items });
+    return res.json({ point: serializedPoint, items });
   }
 
-   async create (req: Request, res: Response){
+  async create(req: Request, res: Response) {
     const {
       name,
       email,
@@ -51,7 +63,7 @@ class PointsController {
     } = req.body;
 
     const point = {
-      image: 'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+      image: req.file.filename,
       name,
       email,
       whatsapp,
@@ -60,24 +72,27 @@ class PointsController {
       city,
       uf
     }
-    
+
     const trx = await knex.transaction();
-  
+
     const insertedIds = await trx('points').insert(point);
-  
+
     const point_id = insertedIds[0];
-  
-    const pointItems = items.map((item_id: number) => {
-      return {
-        item_id,
-        point_id
-      }
-    })
-  
+
+    const pointItems = items
+      .split(',')
+      .map((item: string) => Number(item.trim()))
+      .map((item_id: number) => {
+        return {
+          item_id,
+          point_id
+        }
+      })
+
     await trx('point_items').insert(pointItems);
 
     await trx.commit();
-  
+
     return res.json({
       id: point_id,
       ...point,
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import express, { response } from 'express';
 import cors from 'cors';
 import path from 'path';
 import routes from './routes';
+import { errors } from 'celebrate';
 
 // O express não trás junto a definição de tipos no pacote
 // Precisamos instalar uma dependência para colocar tipagem no express 
@@ -14,5 +15,6 @@ app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
+app.use(errors());
 
 app.listen(3333);
